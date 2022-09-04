57b10451ad119f622eb51e4e6f9a3f3fc369e57f
Shreya Chakraborty <shreyatushi@gmail.com>
2022-09-04 21:14:38 +0530

    This is the first commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
